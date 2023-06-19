2d05d3a9f071638f76c8081d86492dba37ac23d9
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-19 14:51:21 -0400

    added classes and instances of classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,31 @@
+class Product {
+    title = 'DEFAULT';
+    imageUrl;    
+    description;
+    price;
+
+    constructor(title, image, desc, price){
+        this.title = title;
+        this.imageUrl = image;
+        this.description = desc;
+        this.price = price;
+    }
+}
+
 const productList = {
     products: [
-        {
-            title: 'A Pillow',
-            imageUrl: 'http://tiny.cc/en48vz',
-            price: 19.99,
-            description: 'A Soft Pillow'
-        },
-        {
-            title: 'A Rug',
-            imageUrl: 'http://tiny.cc/co48vz',
-            price: 89.99,
-            description: 'We Buid To Your Design'
-        }
+        new Product(
+            'A Pillow', 
+            'http://tiny.cc/en48vz', 
+            'A Soft Pillow', 
+            19.99
+        ),
+        new Product(
+            'A Rug', 
+            'http://tiny.cc/co48vz',
+            'We Buid To Your Design',
+            89.99
+        )
     ],
 
     render() {
